ff01b27a68de775bda5dcd63e8f02b995fbc33c7
Dennis Lau <dennislzf@gmail.com>
2015-07-08 11:42:14 -0700

    I forgot to commit

---
+++ button-schema.js
@@ -0,0 +1,22 @@
+/**
+ * Describes the data expected by a component’s dust template
+ *
+ * We express this using js-schema, a format for describing and validating
+ * object data.
+ */
+
+define(function(require) {
+    var schema = require('js-schema');
+
+    return schema({
+        '?class': String,
+        '?id': String,
+        '?element': [Object, Function, String],
+        '?disabled': Boolean,
+        '?link': String,
+        '?buttonText': String,
+        '?buttonType': String,
+        '?isSecondary': Boolean,
+        '?buttonTextAlign':String
+    });
+});
